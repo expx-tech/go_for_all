c4f7e6ea48db4106221b1c3380e57ba2385e9517
a.aleksandrov <a.aleksandrov@automacon.ru>
2023-10-03 17:57:34 +0300

    сервис

--- my_module.go
+++ my_module.go
@@ -1,7 +1,21 @@
 package go_for_all
 
-import "fmt"
+import (
+	"fmt"
 
-func Version() {
-	fmt.Println("Version v2.0.0")
+	"github.com/expx-tech/go_for_all/v2/app/service"
+)
+
+func Request(token, method string) {
+	if token != "супер_секрет" {
+		fmt.Println("Пользователь не авторизован")
+		return
+	}
+
+	switch method {
+	case "GetUsers":
+		service.GetUsers()
+	default:
+		fmt.Println("Нет такого метода")
+	}
 }

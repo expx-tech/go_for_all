910367ca60856123569a2503c4c4b689e80392f8
a.aleksandrov <a.aleksandrov@automacon.ru>
2023-10-03 17:57:34 +0300

    service

---
+++ app/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+import "fmt"
+
+func GetUser() {
+	fmt.Println("Получение всех пользователей")
+}
--- my_module.go
+++ my_module.go
@@ -1,7 +1,26 @@
 package go_for_all
 
-import "fmt"
+import (
+	"fmt"
 
+<<<<<<< HEAD
 func Version() {
 	fmt.Println("Version v1.1.0")
+=======
+	"github.com/expx-tech/go_for_all/v2/app/service"
+)
+
+func Request(token, method string) {
+	if token != "супер_секрет" {
+		fmt.Println("Пользователь не авторизован")
+		return
+	}
+
+	switch method {
+	case "GetUsers":
+		service.GetUsers()
+	default:
+		fmt.Println("Нет такого метода")
+	}
+>>>>>>> c4f7e6e (сервис)
 }
